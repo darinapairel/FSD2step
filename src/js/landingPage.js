5b8287297b665dd46131e5dd0c72f1af9872b019
Darina Pairel <darinamandarina@users.noreply.github.com>
2019-09-19 13:49:53 +0200

    day 20
     end of components(UI kit), start of Landing

---
+++ src/js/landingPage.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.mainSlider').slick({
+        slidesToShow: 1,
+        variableWidth: true,
+        autoplay:true,
+        autoplaySpeed:5000,
+        arrows:false,
+    });
+});
