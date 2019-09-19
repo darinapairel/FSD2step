a3e593ba02b16ad8096db2ffb85bcd8c6d54a490
Darina Pairel <darinamandarina97@gmail.com>
2019-09-19 18:29:39 +0200

    day 20

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.mainSlider').slick({
         slidesToShow: 1,
+        centerMode: true,
         variableWidth: true,
         autoplay:true,
         autoplaySpeed:5000,
