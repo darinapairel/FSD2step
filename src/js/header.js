4193a86ffe8df0e04d0e88feb95832cedb4a5059
yoleo <yoleo@yandex.ru>
2019-09-20 16:48:43 +0200

    day21 adaptive LP header menu style changes

--- src/js/header.js
+++ src/js/header.js
@@ -3,11 +3,14 @@ $(document).ready(function() {
         $('nav').addClass('menu-block');
         $('#header-menu').show();
         $('#burgeer').hide();
+        $('#close').show();
     });
     
     $('#close').on('click', function(){
        $('nav').removeClass('menu-block');
        $('#header-menu').hide();
+       $('#burgeer').show();
+       $('#close').hide();
 
     });
 });
