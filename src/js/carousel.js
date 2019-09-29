6f4ba2cee520204721d656b80ba2e9d077983667
Darina <darinamandarina97@gmail.com>
2019-09-29 23:17:51 +0200

    day 31 rating

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,15 +1,15 @@
 import "../../libs/slick-1.8.1/slick/slick"
 import "../../libs/rater-master/rater"
 $(document).ready(function(){
-    $('.carousel').slick({
+    $('.carousel__slider').slick({
         slidesToShow: 1,
         fade: true,
         arrows: true,
         dots: true,
-        appendArrows:$(".carousel")
+        appendArrows:$(".carousel__slider")
     });
-    $('.room-star').rate({
-        max_value:5,
-        readonly: true
-      });
+    // $('.room-star').rate({
+    //     max_value:5,
+    //     readonly: true
+    //   });
 })
