3cd5d7e3485b8678d8ff3cd2a92512d716e450ae
Pairel Darina <paireldarina@gmail.com>
2021-10-11 19:44:25 +0300

    New prod branch created

--- src/js/inpMask.js
+++
@@ -1,8 +0,0 @@
-import "../../libs/jquery.maskedinput-master/src/jquery.maskedinput.js";
-
-$(document).ready(function(){
-    $(".date").mask(
-        "99.99.9999",
-        {placeholder:"ДД.MМ.ГГГГ"}
-    );
-})
